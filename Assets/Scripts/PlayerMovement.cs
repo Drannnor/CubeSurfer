9b627a4cfd3ded5197cc7b18da20c92acabdf6ec
Drannnor <chicharo.alexandre@gmail.com>
2022-02-09 22:05:03 +0000

    improved touch controls responsivenes

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,18 +7,20 @@ public class PlayerMovement : MonoBehaviour{
     private Rigidbody _rigidbody;
     [SerializeField] private float velocityMultiplier;
     [SerializeField] private float sideLimit;
+
+    private int _screenWidth;
     
     void Awake(){
         _rigidbody = GetComponent<Rigidbody>();
         if (_rigidbody == null) {
             Debug.Log("ERROR: no rigidbody found");
         }
+
+        _screenWidth = Screen.width;
     }
     void Start(){
     }
-
-   
-
+    
     // Update is called once per frame
     void Update(){
         if (Input.touchCount > 0) {
@@ -26,24 +28,12 @@ public class PlayerMovement : MonoBehaviour{
 
             // Move the player if the finger is moving
             if (touch.phase == TouchPhase.Moved) {
-                Vector2 fingerMovementDirection = touch.deltaPosition;
-    
-                // fingerMovementDirection.y = 0.0f;
-                // Vector2 pos = touch.position;
-                // touch.
-                // pos.x = (pos.x - width) / width;
-                // pos.y = (pos.y - height) / height;
-                // position = new Vector3(-pos.x, pos.y, 0.0f);
-                //
-                // // Position the cube.
-                // transform.position = position;
-                // Vector3 direction = ; 
-                // _rigidbody.velocity = velocityDirection.normalized * velocityMultiplier;
-
-                // transform.position += transform.right * (fingerMovementDirection.x * Time.deltaTime);
+                Vector2 fingerMovement = touch.deltaPosition;
+                float movement = touch.deltaPosition.x / _screenWidth;
                 
-                transform.Translate(fingerMovementDirection.x * Time.deltaTime * velocityMultiplier, 0.0f, 0.0f);
-
+                transform.Translate(movement * velocityMultiplier, 0.0f, 0.0f);
+                
+                Debug.Log($"screen: {_screenWidth}, deltaX:{touch.deltaPosition.x}  movement: {movement}");
                 Vector3 position = transform.position;
                 float limitedX = Mathf.Clamp(position.x, -sideLimit, sideLimit);
                 transform.position = new Vector3(limitedX, position.y, position.z);
