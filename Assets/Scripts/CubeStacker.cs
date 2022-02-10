d832ed9481b97a560675c27a6d8ebcbd5ce83f47
Drannnor <chicharo.alexandre@gmail.com>
2022-02-10 17:42:55 +0000

    implemeted stackalbe cubes

--- Assets/Scripts/CubeStacker.cs
+++ Assets/Scripts/CubeStacker.cs
@@ -1,13 +1,41 @@
 using UnityEngine;
 
-public class BlockStacker : MonoBehaviour{
+public class CubeStacker : MonoBehaviour{
 
+    [SerializeField] private GameObject cubePrefab;
+    
     private int _cubeCount;
+    
     // Start is called before the first frame update
-    void Start(){
+    void Awake(){
+        
     }
 
     // Update is called once per frame
     void Update(){
+        // Debug.Log("update");
+
+        if (Input.GetButtonDown("Jump")) {
+            AddCube(1);
+        }
     }
+
+    public void AddCube(int amount){
+        
+        for (int i = 0; i < amount; i++) {
+            _cubeCount += amount;
+            //move player up
+            transform.Translate(Vector3.up);
+        
+            GameObject cube = Instantiate(cubePrefab, transform, false);
+            //first cube offset is 1.5 successive cubes will have an offset of 1.0f
+            var cubeOffset = Vector3.down * (1.5f + _cubeCount - 1);
+            cube.transform.localPosition += cubeOffset;
+        }
+    }
+
+    public void SubtractCubes(int amount){
+        
+    }
+    
 }
