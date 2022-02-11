6e3c3133a167404db5368e6de8ecfa29e272e44b
Drannnor <chicharo.alexandre@gmail.com>
2022-02-11 18:52:14 +0000

    leve menu will display locked levels has greydout

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -1,28 +1,50 @@
-using System;
+using TMPro;
+using UnityEditor.Timeline;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour {
-
     [SerializeField] private GameObject mainMenu;
     [SerializeField] private GameObject levelSelect;
+    [SerializeField] private TMP_ColorGradient lockedGradient;
+    [SerializeField] private TMP_ColorGradient unlockedGradient;
+    private const string LEVEL_BUTTON = "LevelButton";
+
     public void LoadLevel(string levelName) {
         SceneManager.LoadScene(levelName);
     }
 
-    public void OnEnable() {
+    private void ChangeLevelButtonColors() {
         var index = GameManager.GM.GetCurrentLevelIndex();
+        Debug.Log($"Index: {index}");
+        var levelSelectMenu = levelSelect.transform;
+        for (int i = 0; i < levelSelectMenu.childCount; i++) {
+            var levelButton = levelSelectMenu.GetChild(i);
+            if (!levelButton.CompareTag(LEVEL_BUTTON)) return;
+            var gradient = i <= index ? unlockedGradient : lockedGradient;
+            ChangeGradient(levelButton, gradient);
+        }
+    }
+
+    private void ChangeGradient(Transform levelButton, TMP_ColorGradient gradient) {
+        var text = levelButton.GetChild(0).GetComponent<TMP_Text>();
+        if (text != null) {
+            text.colorGradientPreset = gradient;
+        } else {
+            Debug.Log("ERROR: No textMeshPro Found");
+        }
     }
 
     public void Quit() {
         Application.Quit();
     }
-    
+
     public void OpenLevelSelection() {
         mainMenu.SetActive(false);
         levelSelect.SetActive(true);
+        ChangeLevelButtonColors();
     }
-    
+
     public void GoBackToMenu() {
         mainMenu.SetActive(true);
         levelSelect.SetActive(false);
